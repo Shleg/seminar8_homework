a4708614a87a55512206f7d1905206135db9c9cc
Andrew Shlegel <a.shlegel@outlook.com>
2023-03-21 19:09:41 -0300

    Task056 added

---
+++ Task056/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+// с наименьшей суммой элементов: 1 строка
+
+int[,] GetRandomMatrix(int rows, int columns, int leftBorder, int rightBorder)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{ 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int FindMinRowsSum(int[,] matrix)
+{
+    int[] array = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[i] += matrix[i, j];  
+        }
+    }
+    return Array.IndexOf(array, array.Min());
+}
+
+void Main()
+{
+    const int ROWS = 4;
+    const int COLUMNS = 6;
+    const int LEFTBORDER = 0;
+    const int RIGHTBORDER = 9;
+    int[,] myMatrix = GetRandomMatrix(ROWS, COLUMNS, LEFTBORDER, RIGHTBORDER);
+    PrintMatrix(myMatrix);
+    Console.WriteLine();
+    Console.WriteLine($"Номер строки с наименьшей суммой равен {FindMinRowsSum(myMatrix) + 1}");
+}
+
+Main();
